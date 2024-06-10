a62e5174042f32e90bdd0faa43cf51d0ff0f45b7
tham@3f.team <E4as73NGDt2JUP7kc4ye>
2024-06-10 15:56:19 +0700

    Ecommer Video #1-3

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('redirect', [HomeController::class, 'redirect']);
